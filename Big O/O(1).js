276d697db4a98478a6a6e8305b93765e7594d2c5
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-17 02:25:46 +0530

    Add files via upload

---
+++ Big O/O(1).js
@@ -0,0 +1,4 @@
+//O(1) refers to constant time
+//It reffers that number of operations will be constant regardless of the size of the data
+//if numbers of operations is 1 then the time complexity is O(1) if its 2 then O(2) and so on
+//O(1) gives a line parallel to the x axis in the graph of elements to no of operations
