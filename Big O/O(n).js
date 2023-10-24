70a2d116eacc1f3b583620fd2aa81935887b86a9
bedanta69 <116745791+bedanta69@users.noreply.github.com>
2023-10-25 01:05:09 +0530

    Add files via upload

--- Big O/O(n).js
+++ Big O/O(n).js
@@ -1,4 +1,4 @@
 //O(n) refers to linear time 
-//it basically refers to that number of operations for will be same as number of i(nput 
+//it basically refers to that number of operations will be same as number of input 
 //O(n) gives a straight line passing through the origin in the graph of number of elements to number of operations
 
