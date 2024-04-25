a52ffc944f2066ac24240348774584ad60f006db
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-04-25 20:46:14 +0530

    Add files via upload

--- Big O/O(n).js
+++ Big O/O(n).js
@@ -1,4 +1,15 @@
 //O(n) refers to linear time 
 //it basically refers to that number of operations will be same as number of input 
 //O(n) gives a straight line passing through the origin in the graph of number of elements to number of operations
+//example of such a code:
 
+let a = [1, 2, 3, 4, 5]
+
+function find5(input) {
+    for (i = 0; i < input.length; i++) {
+        if (input[i] == 5) {
+            console.log('found 5')
+        }
+    }
+}
+find5(a)
