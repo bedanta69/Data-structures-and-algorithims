488e8b22541b1b17d8f6c69a1c9faffb0b5d9228
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-09-01 23:59:47 +0530

    Delete Big O directory

--- Big O/calculationrule.js
+++
@@ -1,5 +0,0 @@
-// Rules for calculating Big O
-// Rule 1 : Worst Case
-// Rule 2 : Remove comstants
-// Rule 3 : Different term for Inputs
-// Rule 4 : Drop Non Dominants
--- Big O/rule4.js
+++
@@ -1,3 +0,0 @@
-//drop non dominant terms
-//if a code consists of o(n) and o(n^2) notations then then the dominent term is o(n^2)
-//so the code is of o(n^2) notation
