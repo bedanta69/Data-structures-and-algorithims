8dbc5586461f33689a9fc5f856bdd764b298c734
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-09-02 00:10:25 +0530

    Add files via upload

---
+++ essential_c_and_c++_concepts/array_basics.c
@@ -0,0 +1,14 @@
+/*creation, initilization, scanning  of an array */
+#include<stdio.h>
+int main(){
+	int A[5];  /*creation of the array of size 5  A*/
+	int B[4] = {1,2,3,4,};  /*initialization and creation of an array B*/
+	for(int i=0;i<4;i++){
+		printf("%d",B[i]);
+	} /*scanning through each element of the array and printing it*/
+
+        return 0;
+
+}
+
+
