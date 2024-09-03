7078689b1b82a935cc403cccaa9c25ac9f67567c
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-09-03 07:04:20 +0530

    Add files via upload

---
+++ essential_c_and_c++_concepts/reference.cpp
@@ -0,0 +1,8 @@
+/*Reference is an alias or nickname given to a variable*/
+/*it is useful in parameter passing*/
+#include<iostream>
+int main(){
+    int a =10;
+    int &r = a;/*r can be used as a*/
+    return 0;
+}
