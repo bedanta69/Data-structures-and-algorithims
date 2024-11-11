33cdd72bfc5c4d3c7abe8e08f0670cefabd2aba2
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2024-11-11 09:49:06 +0530

    Add files via upload

--- essential_c_and_c++_concepts/parameterpass2.cpp
+++ essential_c_and_c++_concepts/parameterpass2.cpp
@@ -1,19 +1,18 @@
-//call by value (parameters in the swap function are pointers)
-#include<iostream>
+// call by address (parameters in the swap function are pointers)
+#include <iostream>
 using namespace std;
-void swap(int *x, int *y){
+void swap(int *x, int *y)
+{
 	int temp;
 	temp = *x;
 	*x = *y;
 	*y = temp;
 }
-int main(){
-	int num1 =10, num2 =15;
-	swap(&num1,&num2);
-	cout<<"the first number"<<num1<<endl;
-	cout<<"the second number"<<num2<<endl;
+int main()
+{
+	int num1 = 10, num2 = 15;
+	swap(&num1, &num2);
+	cout << "the first number" << num1 << endl;
+	cout << "the second number" << num2 << endl;
 	return 0;
 }
-	
-
-
