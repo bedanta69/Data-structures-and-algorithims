da5401dc67fe05c003bc29d73d717aaf932d06d8
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2025-07-07 17:24:47 +0530

    Add files via upload

--- oop/class_object.cpp
+++ oop/class_object.cpp
@@ -5,17 +5,29 @@
 using namespace std;
 class Teacher
 {
+private:
+  double salary;
+
 public: // Access modifier
   // properties/ attributes
   string name;
   string dept;
   string subject;
-  float salary;
 
   // methods/ member functions
   void changeDeppt(string newDept)
   {
     dept = newDept;
+  }
+  // setter function[set (update) the value of a private variable.]
+  void setSalary(double s)
+  {
+    salary = s;
+  }
+  // getter function[Getter: get (read) the value of a private variable.]
+  double getSalary()
+  {
+    return salary;
   }
 };
 
@@ -25,7 +37,8 @@ int main()
   T1.name = "Bedanta";
   T1.subject = "chess";
   T1.dept = "Information Technology";
-  T1.salary = 2500;
+  T1.setSalary(25000);
+  cout << T1.getSalary() << endl;
   cout << T1.name << endl;
   return 0;
 }
