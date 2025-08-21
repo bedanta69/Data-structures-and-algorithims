7e8434d5681dd80db64f785de1b7a4db12308fe7
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2025-08-21 19:54:39 +0530

    Add files via upload

---
+++ miscelenous/Bitwise3.cpp
@@ -0,0 +1,9 @@
+// Bitwise operator : Bitwise ^ (XOR)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    int b = 4;
+    cout << (a ^ b) << endl; // 0 XOR of same number is 0
+}
