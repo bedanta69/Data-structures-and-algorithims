14f74c60e1373a97b851970297ddcc83398114aa
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2025-08-21 20:09:25 +0530

    Add files via upload

---
+++ miscelenous/Bitwise4.cpp
@@ -0,0 +1,8 @@
+// Bitwise operator: Bitwise << [left shift operator]
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    cout << (a << 1) << endl;
+}
---
+++ miscelenous/Bitwise5.cpp
@@ -0,0 +1,8 @@
+// Bitwise right shift operator
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    cout << (a >> 1) << endl;
+}
